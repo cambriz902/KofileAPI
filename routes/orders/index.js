6642a0a3c254f10bdab104d409b4a3f35dffcf07
christian <christian.ambriz@gmail.com>
2017-01-06 19:47:23 -0800

    clean code up

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -1,12 +1,9 @@
 const orders = require('express').Router();
-
-/* Orders Routes */
 const prices = require('./prices');
 const funds_distribution = require('./funds_distribution');
 
-
+/* Order Routes */
 orders.post('/prices', prices);
 orders.post('/funds_distribution', funds_distribution);
-
 
 module.exports = orders;
--- routes/orders/services/utils/findOrderTypeData.js
+++ routes/orders/services/utils/findOrderTypeData.js
@@ -1,6 +1,5 @@
 const fees_and_distributions_data = require('../../../../data/fees.json');
 
-
 module.exports = item_type => {
   const order_type_data = fees_and_distributions_data.find(data => {
     return data.order_item_type === item_type
